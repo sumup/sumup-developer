73eab93c69654fc5b82f3fdfc2a49f8301a6d2f2
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-01-30 09:13:56 +0100

    fix: missing env defs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly MARKETING_CLOUD_AUTH: string;
+  readonly MARKETING_CLOUD_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
