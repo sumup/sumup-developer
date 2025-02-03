564668f1c749a67b0a52b69d5e139bfea315a7d0
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-02-03 08:56:50 +0100

    feat: improve homepage 'try it out' code

--- ec.config.mjs
+++ ec.config.mjs
@@ -8,7 +8,6 @@ export default {
     borderColor: "var(--cui-border-subtle)",
     borderRadius: "var(--cui-border-radius-byte)",
     borderWidth: "var(--cui-border-width-kilo)",
-    codeFontWeight: "var(--cui-font-weight-semibold)",
     codeFontFamily: "var(--cui-font-stack-mono)",
     frames: {
       editorTabBarBorderBottomColor: "var(--cui-border-subtle)",
