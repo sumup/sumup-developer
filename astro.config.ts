83ed3e34954649320ca07d65f13c2dbf161edad2
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-02-05 21:22:43 +0100

    fix: google analytics setup, attempt 2

---
+++ astro.config.ts
@@ -0,0 +1,209 @@
+import markdoc from "@astrojs/markdoc";
+import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
+import vercel from "@astrojs/vercel";
+import starlightLinksValidator from "starlight-links-validator";
+
+import { defineConfig } from "astro/config";
+import type { HeadUserConfig } from "node_modules/@astrojs/starlight/schemas/head";
+
+const head = (): HeadUserConfig => {
+  const head: HeadUserConfig = [
+    {
+      tag: "link",
+      attrs: {
+        rel: "preload",
+        href: "https://static.sumup.com/fonts/Inter/Inter-normal-latin.woff2",
+        as: "font",
+        type: "font/woff2",
+        crossorigin: true,
+      },
+    },
+    {
+      tag: "link",
+      attrs: {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "https://static.sumup.com/favicons/apple-touch-icon.png",
+      },
+    },
+    {
+      tag: "link",
+      attrs: {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "https://static.sumup.com/favicons/favicon-32x32.png",
+      },
+    },
+    {
+      tag: "link",
+      attrs: {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "https://static.sumup.com/favicons/favicon-16x16.png",
+      },
+    },
+    {
+      tag: "link",
+      attrs: {
+        rel: "manifest",
+        href: "https://static.sumup.com/favicons/site.webmanifest",
+      },
+    },
+    {
+      tag: "link",
+      attrs: {
+        rel: "mask-icon",
+        href: "https://static.sumup.com/favicons/safari-pinned-tab.svg",
+        color: "#ffffff",
+      },
+    },
+    {
+      tag: "link",
+      attrs: {
+        rel: "shortcut icon",
+        href: "https://static.sumup.com/favicons/favicon.ico",
+      },
+    },
+    {
+      tag: "meta",
+      attrs: {
+        name: "msapplication-TileColor",
+        content: "#171d24",
+        media: "(prefers-color-scheme: dark)",
+      },
+    },
+    {
+      tag: "meta",
+      attrs: { name: "msapplication-TileColor", content: "#fff" },
+    },
+    {
+      tag: "meta",
+      attrs: {
+        name: "msapplication-config",
+        content: "https://static.sumup.com/favicons/browserconfig.xml",
+      },
+    },
+    {
+      tag: "meta",
+      attrs: {
+        name: "theme-color",
+        content: "#171d24",
+        media: "(prefers-color-scheme: dark)",
+      },
+    },
+    { tag: "meta", attrs: { name: "theme-color", content: "#fff" } },
+    {
+      tag: "script",
+      attrs: {
+        src: "https://static.sumup.com/legacy-browsers/check-support.js",
+        defer: true,
+      },
+      content: "",
+    },
+  ];
+
+  if (import.meta.env.PUBLIC_ONETRUST_DOMAIN_ID) {
+    head.push({
+      tag: "script",
+      attrs: {
+        defer: true,
+        charset: "UTF-8",
+        src: "https://cdn-ukwest.onetrust.com/scripttemplates/otSDKStub.js",
+        "data-domain-script": import.meta.env.PUBLIC_ONETRUST_DOMAIN_ID,
+      },
+      content: "",
+    });
+  }
+
+  if (import.meta.env.PUBLIC_GA_TAG_ID) {
+    head.push({
+      tag: "script",
+      attrs: {
+        src: `https://www.googletagmanager.com/gtag/js?id=${import.meta.env.PUBLIC_GA_TAG_ID}`,
+      },
+      content: "",
+    });
+    head.push({
+      tag: "script",
+      attrs: {},
+      content: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${import.meta.env.PUBLIC_GA_TAG_ID}');
+      `,
+    });
+  }
+
+  return head;
+};
+
+export default defineConfig({
+  adapter: vercel(),
+  site: "https://developer.sumup.com",
+
+  experimental: {
+    contentIntellisense: true,
+  },
+
+  integrations: [
+    markdoc({ allowHTML: true }),
+    react(),
+    starlight({
+      plugins: [starlightLinksValidator()],
+      title: "SumUp Developer",
+      favicon: "favicon.png",
+      disable404Route: true,
+      sidebar: [
+        {
+          label: "online-payments",
+          autogenerate: { directory: "online-payments" },
+        },
+        {
+          label: "terminal-payments",
+          autogenerate: { directory: "terminal-payments" },
+        },
+        // NOTE: not rolled out yet
+        // {
+        //   label: "webhook-docs",
+        //   autogenerate: { directory: "webhook-docs" },
+        // },
+        // TODO: convert to similar page to FAQ
+        // {
+        //   label: "problem",
+        //   autogenerate: { directory: "problem" },
+        // },
+      ],
+      head: head(),
+      components: {
+        Footer: "./src/overrides/Footer.astro",
+        Head: "./src/overrides/Head.astro",
+        Hero: "./src/overrides/Hero.astro",
+        MarkdownContent: "./src/overrides/MarkdownContent.astro",
+        Sidebar: "./src/overrides/Sidebar.astro",
+        PageTitle: "./src/overrides/PageTitle.astro",
+        SiteTitle: "./src/overrides/SiteTitle.astro",
+        SocialIcons: "./src/overrides/SocialIcons.astro",
+      },
+      social: {
+        github: "https://github.com/sumup/documentation",
+      },
+      editLink: {
+        baseUrl: "https://github.com/sumup/documentation/edit/main/",
+      },
+      customCss: [
+        "@sumup-oss/circuit-ui/experimental/styles.css",
+        "./src/base.css",
+        "./src/title.css",
+      ],
+      pagination: false,
+      lastUpdated: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
