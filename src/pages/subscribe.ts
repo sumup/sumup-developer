f3755b5ddcb5ecfb8dc972412392a53450d6ea63
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-08-04 14:01:20 +0200

    chore: cleanup supplementary files

--- src/pages/subscribe.ts
+++ src/pages/subscribe.ts
@@ -1,14 +1,7 @@
 export const prerender = false;
 
+import { validateEmail } from "@lib/validations";
 import type { APIRoute } from "astro";
-
-const validateEmail = (email: string): boolean => {
-  return !!String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    );
-};
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const data = await request.formData();
