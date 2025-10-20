0503b9e3a02f98fe85be8e334e605cfd8d77c547
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-10-20 17:56:24 +0200

    fix(docs): markdown version of pages

---
+++ src/pages/[...path]/index.md.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from "astro";
+import { getCollection, getEntry } from "astro:content";
+
+export const GET: APIRoute = async ({ params }) => {
+  const { path } = params;
+
+  const doc = await getEntry("docs", path || "index");
+  if (!doc) {
+    return new Response(null, {
+      status: 404,
+      statusText: "Not found",
+    });
+  }
+
+  const markdown = `# ${doc.data.title}\n\n` + doc.body;
+
+  return new Response(markdown, {
+    headers: {
+      "content-type": "text/markdown; charset=utf-8",
+    },
+  });
+};
+
+export async function getStaticPaths() {
+  const docs = await getCollection("docs");
+  const paths = docs.map((doc) => ({
+    params: { path: doc.id },
+  }));
+
+  return paths;
+}
