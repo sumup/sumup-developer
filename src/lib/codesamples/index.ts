ce31c640a12bff578e7d178176efd4aaba422679
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-10-27 12:45:51 +0100

    feat(api): rust code samples

--- src/lib/codesamples/index.ts
+++ src/lib/codesamples/index.ts
@@ -2,5 +2,6 @@ import { curl } from "./curl";
 import { go } from "./go";
 import { node } from "./node";
 import { python } from "./python";
+import { rust } from "./rust";
 
-export { curl, go, node, python };
+export { curl, go, node, python, rust };
