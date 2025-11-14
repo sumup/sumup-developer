60525972549b1a5bd1e816c2598c39558afb52a6
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-11-14 17:43:36 +0100

    feat: styling update

--- astro.config.ts
+++ astro.config.ts
@@ -217,6 +217,7 @@ export default defineConfig({
         SocialIcons: "./src/overrides/SocialIcons.astro",
         PageFrame: "./src/overrides/PageFrame.astro",
         PageTitle: "./src/overrides/PageTitle.astro",
+        MobileTableOfContents: "./src/overrides/MobileTableOfContents.astro",
       },
       social: [
         {
