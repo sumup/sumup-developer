09ce4b07402d34b2bdb0ad6daf32e5c6bf775aa7
Grzegorz Kossobudzki <grzeynick@gmail.com>
2025-12-03 13:54:18 +0100

    Variables for product names (#366)
    * Variables for product names
    
    * removed variables from headers
    
    * link fixes
    
    * link fixes pt2

---
+++ src/lib/productnames.ts
@@ -0,0 +1,5 @@
+export const iossdkname = "iOS Terminal SDK";
+export const androidreadersdkname = "Android Reader SDK";
+export const androidttpsdkname = "Android Tap-to-Pay SDK";
+export const cloudapiname = "Cloud API";
+export const companyname = "SumUp"
