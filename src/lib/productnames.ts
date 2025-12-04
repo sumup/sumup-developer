30a8dfe8bab2a5f6969de278bb3f9107b1ea328a
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-12-04 18:45:37 +0100

    chore: simplify icons, cleanup, format

--- src/lib/productnames.ts
+++ src/lib/productnames.ts
@@ -2,4 +2,4 @@ export const iossdkname = "iOS Terminal SDK";
 export const androidreadersdkname = "Android Reader SDK";
 export const androidttpsdkname = "Android Tap-to-Pay SDK";
 export const cloudapiname = "Cloud API";
-export const companyname = "SumUp"
+export const companyname = "SumUp";
