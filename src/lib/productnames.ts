b7033b915082901a696f01319f7c7fa44c84c982
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-03-06 11:52:29 +0000

    chore: remove product names
    Hardcode the values, there shouldn't be a need to have
    these extracted into re-usable vars as it makes the raw
    markdown / textual representation of the articles unclear.

--- src/lib/productnames.ts
+++
@@ -1,5 +0,0 @@
-export const iossdkname = "iOS Terminal SDK";
-export const androidreadersdkname = "Android Reader SDK";
-export const androidttpsdkname = "Android Tap-to-Pay SDK";
-export const cloudapiname = "Cloud API";
-export const companyname = "SumUp";
