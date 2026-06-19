417da8425cf93eab742d6acde72cbb6c7066d727
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-06-19 10:21:28 +0200

    fix: codeblocks background

--- ec.config.mjs
+++ ec.config.mjs
@@ -11,7 +11,7 @@ export default defineEcConfig({
     borderRadius: "0",
     borderWidth: "0",
     codeFontFamily: "var(--cui-font-stack-mono)",
-    codeBackground: "var(--cui-bg-normal)",
+    codeBackground: "var(--cui-bg-subtle)",
     gutterBorderWidth: "0",
     frames: {
       editorTabBarBorderBottomColor: "var(--cui-border-subtle)",
