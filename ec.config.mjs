c6aa2a6530a283d1128ec30b22c12795cc20f284
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-08-14 13:56:39 +0200

    fix: background color of code blocks

--- ec.config.mjs
+++ ec.config.mjs
@@ -11,7 +11,7 @@ export default defineEcConfig({
     borderRadius: "0",
     borderWidth: "0",
     codeFontFamily: "var(--cui-font-stack-mono)",
-    codeBackground: "var(--cui-bg-subtle)",
+    codeBackground: "var(--sl-color-black)",
     gutterBorderWidth: "0",
     frames: {
       editorTabBarBorderBottomColor: "var(--cui-border-subtle)",
