481c60b216151a5fb0945a03a667e6dc9a453a3e
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-08-14 11:29:55 +0200

    feat(api): list endpoints for each tag

---
+++ src/components/ApiDocs/EndpointList.tsx
@@ -0,0 +1,46 @@
+import { ListItemGroup } from "@sumup-oss/circuit-ui";
+import type { OperationObject } from "src/types/openapi";
+
+import styles from "./EndpointList.module.css";
+
+interface Props {
+  operations: OperationObject[];
+  selectedOperationSlug?: string;
+  tagSlug: string;
+}
+
+const EndpointDetails = ({ operation }: { operation: OperationObject }) => {
+  const method = operation.method.toLowerCase();
+
+  return (
+    <span className={styles.endpoint}>
+      <span className={`${styles.method} ${styles[method] ?? ""}`}>
+        {method.toUpperCase()}
+      </span>
+      <span className={styles.path}>{operation.path}</span>
+    </span>
+  );
+};
+
+export default function EndpointList({
+  operations,
+  selectedOperationSlug,
+  tagSlug,
+}: Props) {
+  return (
+    <ListItemGroup
+      label="Endpoints"
+      hideLabel
+      items={operations.map((operation) => ({
+        key: operation.slug,
+        label:
+          operation.summary ??
+          `${operation.method.toUpperCase()} ${operation.path}`,
+        details: <EndpointDetails operation={operation} />,
+        href: `/api/${tagSlug}/${operation.slug}/`,
+        variant: "navigation",
+        selected: selectedOperationSlug === operation.slug,
+      }))}
+    />
+  );
+}
